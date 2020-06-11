25119286b77520aa361354a492fd083bf5c4e1a4
Ola Bäcker <olabacker@gmail.com>
2020-06-11 23:09:37 +0200

    ignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace spark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
