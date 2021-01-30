e192d9d9eeedf16b8fd7545921e0ce70e2324090
Ola Bäcker <olabacker@gmail.com>
2021-01-30 21:47:29 +0100

    copy to clipboard

---
+++ src/Spark/Clipboard/Clipboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Spark.Clipboard
+{
+    public static class Clipboard
+    {
+        public static void SetText(string text)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                WindowsClipboard.SetText(text);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                OsxClipboard.SetText(text);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                LinuxClipboard.SetText(text);
+            }
+        }
+}
